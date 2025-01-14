a01b6f1d5e766f8d71612140d35fea344eb66430
Jonathan Bergos <jbergos@c2r3p6.42lehavre.fr>
2025-01-15 00:50:53 +0100

    ~ modif pointer struct lst_env

--- src/parsing/struct/lst_env.c
+++ src/parsing/struct/lst_env.c
@@ -6,7 +6,7 @@
 /*   By: jbergos <jbergos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 22:24:00 by jbergos           #+#    #+#             */
-/*   Updated: 2025/01/15 00:28:43 by jbergos          ###   ########.fr       */
+/*   Updated: 2025/01/15 00:47:56 by jbergos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,20 +34,23 @@ t_env	*create_lst_env(char **envp)
 	lst_env = NULL;
 	while (envp[i])
 	{
-		lst_env_add_last(lst_env, create_env(envp[i]));
+		lst_env_add_last(&lst_env, create_env(envp[i]));
 		++i;
 	}
 	return (lst_env);
 }
 
-void	lst_env_add_last(t_env *lst_env, t_env *env)
+void	lst_env_add_last(t_env **lst_env, t_env *env)
 {
 	t_env	*tmp;
 
-	if (!(lst_env))
-		lst_env = env;
+	if (!(*lst_env))
+	{
+		*lst_env = env;
+		return ;
+	}
 	
-	tmp = lst_env;
+	tmp = *lst_env;
 	while (tmp)
 	{
 		if (!tmp->next)
@@ -62,7 +65,10 @@ void	show_lst_env(t_env *lst_env)
 	t_env	*tmp;
 
 	if (!(lst_env))
+	{
+		printf("her\n");
 		return ;
+	}
 	tmp = lst_env;
 	while (tmp)
 	{

117df496af90dbff479849274da609d2a3430753
Milan Kaliszczak <mkaliszc@c2r4p6.42lehavre.fr>
2025-01-14 03:32:43 +0100

    + | struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mkaliszc <mkaliszc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 02:49:05 by albillie          #+#    #+#             */
-/*   Updated: 2025/01/14 02:49:28 by albillie         ###   ########.fr       */
+/*   Updated: 2025/01/14 03:32:10 by mkaliszc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,32 @@
 #include "jojo.h"
 #include "milan.h"
 #include "alban.h"
+
+typedef	enum s_tokens
+{
+	WORD_TOK,
+	CMD_TOK,
+	IN_TOK,
+	OUT_TOK,
+	APP_TOK,
+	HDC_TOK,
+}		t_type;
+
+typedef struct s_index
+{
+	int	in;
+	int	out;
+	int	append;
+	int	here_d;
+}			t_index;
+
+typedef struct s_env
+{
+	char			*env;
+	struct s_env	*next;
+}			t_env;
+
+typedef	struct s_mini
+{
+	struct s_env	**env;
+}				t_mini;

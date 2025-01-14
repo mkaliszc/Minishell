fb309326618d40fa537dfc0a856d0690725677e2
Milan Kaliszczak <mkaliszc@c2r4p6.42lehavre.fr>
2025-01-14 02:31:18 +0100

    test

--- OctoLIB/src/lst/ft_lstdelone.c
+++ OctoLIB/src/lst/ft_lstdelone.c
@@ -1,16 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkaliszc <mkaliszc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/13 22:33:21 by mkaliszc          #+#    #+#             */
-/*   Updated: 2025/01/13 23:20:33 by mkaliszc         ###   ########.fr       */
+/*   Created: 2024/10/19 17:24:55 by mkaliszc          #+#    #+#             */
+/*   Updated: 2024/10/19 17:46:13 by mkaliszc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef struct minishell
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	int i;
-};
+	if (!del)
+		return ;
+	del(lst -> content);
+	free(lst);
+}

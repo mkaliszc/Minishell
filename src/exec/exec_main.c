546fb20c8e6f9361c4037839fcc2f8555c4735ec
Milan Kaliszczak <mkaliszc@c2r2p6.42lehavre.fr>
2025-01-15 03:38:51 +0100

    + | start of redirection and some comments on what to do

--- src/exec/exec_main.c
+++ src/exec/exec_main.c
@@ -6,7 +6,7 @@
 /*   By: mkaliszc <mkaliszc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 21:51:01 by mkaliszc          #+#    #+#             */
-/*   Updated: 2025/01/15 01:45:07 by mkaliszc         ###   ########.fr       */
+/*   Updated: 2025/01/15 03:37:48 by mkaliszc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,20 @@
 
 /*
 	TODO : executing checklist
-	? check parameter function
 	* check where to redirect the entry and if we need, redirect the output
 	* If we have an here_doc do it first
 	! ulimit -n (if we open to much fd)
 	* find path to cmd and execve
 	* do it for every pipe
-*/
 
+	todo bis : if we have only one cmd (no pipe)
+	? redirecting then check if we have a builtins (for the parent)
+	? if no then fork, redirect the child output and execve
+
+	todo : multiple pipes
+	? pipex but need some modifications
+
+*/
 void	which_builtins(t_mini *data)
 {
 	if (ft_strcmp("env", *data->lst_cmd->cmd[0]) == 0)
@@ -42,33 +48,18 @@ void	which_builtins(t_mini *data)
 
 void	executing_minishell(t_mini *data)
 {
-	t_order	*cur;
-	int		status;
-	char	*infile;
+	t_order_file	*cur;
+	char			*infile;
 
-	cur = data->cmd;
-	if (data->lst_cmd->is_builtins == true && data->nb_cmd == 1)
+	cur = data->lst_cmd->order_file;
+	if (data->lst_cmd->is_builtins == true && data->nb_cmd == 1) // ! we need to do this after redirecting output and input
 		which_builtins(data);
-	status = 0;
 	while (cur)
 	{
-		if (status == 0 && cur->type == hdc)
-		{
-			infile = handle_heredoc();
-			if (cur->next == NULL)
-			{
-				cur = data->order;
-				status = 1;
-			}		
-		}
-		else if (status == 1 && cur->type == in)
-		{
-			infile = handle_heredoc();
-			if (cur->next == NULL)
-			{
-				cur = data->order;
-				status = 1;
-			}		
-		}
-	}	
+		if (cur->type == HDC)
+			infile = handle_here_doc(cur->file);
+		else if (cur->type == IN)
+			infile = cur->file;
+		cur = cur->next;
+	}
 }

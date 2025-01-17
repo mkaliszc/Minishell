f3908bcf900b73df80472b6596674187930e3b33
kaveO <alban.billiette@gmail.com>
2025-01-17 10:32:06 +0100

    ~ | Work in progress on fixing leaks for builtins

--- src/builtins/main.c
+++ src/builtins/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kaveo <kaveo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 08:04:18 by albillie          #+#    #+#             */
-/*   Updated: 2025/01/16 14:57:04 by albillie         ###   ########.fr       */
+/*   Updated: 2025/01/17 10:31:17 by kaveo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ int main(int ac, char **av, char **envp)
 	(void)ac;
 	(void)av;
 	char *line;
+	(void)envp;
 	env = create_lst_env(envp);
 	while (true)
 	{
@@ -41,26 +42,35 @@ int main(int ac, char **av, char **envp)
 		else if (ft_strcmp(command[0], "cd") == 0)
 		{
 			handle_cd(command, &env);
+			ft_free_char_tab(command);
 		}
 		else if (ft_strcmp(command[0], "env") == 0)
 		{
 			handle_env(env);
+			ft_free_char_tab(command);
 		}
 		else if (ft_strcmp(command[0], "echo") == 0)
 		{
 			handle_echo(command);
+			ft_free_char_tab(command);
 		}
 		else if (ft_strcmp(command[0], "exit") == 0)
 		{
+			ft_free_char_tab(command);
 			break; // ! Have to work on exit command right here.
 		}
 		else if (ft_strcmp(command[0], "export") == 0)
 		{
 			handle_export(command, &env);
+			ft_free_char_tab(command);
 		}
 		else if (ft_strcmp(command[0], "unset") == 0)
 		{
 			handle_unset(command, &env);
+			ft_free_char_tab(command);
 		}
+		else
+			ft_free_char_tab(command);
 	}
+	free_lst_env(env);
 }

a2767885df152bc7342abf0e8724ee5fe50e1e41
kaveOO <alban.billiette@gmail.com>
2025-01-23 21:31:50 +0100

    + | Added export for multiples arguments

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 10:23:29 by albillie          #+#    #+#             */
-/*   Updated: 2025/01/19 05:26:22 by albillie         ###   ########.fr       */
+/*   Updated: 2025/01/23 20:50:54 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,17 @@ int	cmd_array_size(char **array)
 
 void	handle_export(char **cmd, t_env **env)
 {
+	int	i;
+
 	if (cmd_array_size(cmd) < 2)
 		return ;
-	u_r_one_lst_env(*env, cmd[1]);
-	return ;
+	i = 1;
+	while (cmd[i])
+	{
+		u_r_one_lst_env(*env, cmd[i]);
+		i++;
+	}
+
+	// u_r_one_lst_env(*env, cmd[1]);
+	// return ;
 }
